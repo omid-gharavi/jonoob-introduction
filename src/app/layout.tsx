84080f919e67165d78a1838b8ea9843d6300e576
Omid <omidgharavi82@gmail.com>
2026-08-05 23:26:05 +0330

    add: layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,10 @@ const IranYekan = localFont({
 export const metadata: Metadata = {
   title: "جنوب ایران",
   description: "معرفی شهر های جنوب",
-  icons: '/favicon.ico'
+  authors: [{ name: 'Omid Gharavi', url: 'https://github.com/omid-gharavi' }],
+  creator: 'Omid Gharavi',
+  publisher: 'Omid Gharavi',
+  keywords: ['جنوب ایران', 'معرفی شهر های جنوب', 'معرفی شهر های جنوبی ایران', 'جنوب ایران', 'سفر به جنوب ایران', 'سفر به جنوب', 'سفر به جنوب کشور', 'سفر به جنوب کشور ایران', 'سفر به جنوب کشور ایران', 'سفر به جنوب کشور ایران', 'سفر به جنوب کشور ایران', 'سفر به جنوب کشور ایران', 'سفر به جنوب کشور ایران', 'سفر به جنوب کشور ایران', 'سفر به جنوب کشور ایران', 'سفر به جنوب کشور ایران', 'سفر به جنوب کشور ایران'],
 };
 
 export default function RootLayout({
